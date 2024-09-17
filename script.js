211cf9236430c2b2223a1836922475fe12833d61
pelzmagic <akinloyepelz@gmail.com>
2024-09-17 20:57:20 +0100

    add updates to styling

---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const sliderContainer = document.querySelector(".slider_container");
+const cardSlide = document.querySelector(".card_slide");
+const items = Array.from(document.querySelectorAll(".card"));
+console.log(items);
